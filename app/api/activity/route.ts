97810cc706d52312393eda5280de4c220f58568a
Bill Wood <bwood@simbox.io>
2024-03-11 14:35:29 -0500

    Import fix and prisma build command

--- app/api/activity/route.ts
+++ app/api/activity/route.ts
@@ -1,9 +1,8 @@
 // app/api/activity/route.ts
 
 import { NextRequest, NextResponse } from 'next/server';
-import prisma from '../../../lib/prisma';
+import prisma, { User } from '@/lib/prisma';
 import { getSession } from 'next-auth/react';
-import { User } from '@/lib/prisma';
 
 export async function GET(request: NextRequest) {
   const session = await getSession();
