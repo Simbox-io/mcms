eac49f4e5b37f84f7225d42b0e41188194322172
Bill Wood <bwood@simbox.io>
2024-03-14 23:54:43 -0500

    Enable server actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverActions: {
+            allowedOrigins: ['https://vp1ldb7xwny0.statuspage.io'],
+        },
+    },
+};
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
