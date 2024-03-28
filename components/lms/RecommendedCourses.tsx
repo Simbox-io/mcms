d5bafc9be30b80aa1dc22ca314f5cc03f8bb9a59
M1RL0K <mirlok@dr.com>
2024-03-28 09:04:53 -0400

    Update RecommendedCourses.tsx

--- components/lms/RecommendedCourses.tsx
+++ components/lms/RecommendedCourses.tsx
@@ -1,24 +1,37 @@
-// components/RecommendedCourses.tsx
-import { CourseRecommendation, Course } from '@/lib/prisma';
+import { Course, CourseRecommendation } from '@/lib/prisma';
 import CourseCard from './CourseCard';
 
 type RecommendedCoursesProps = {
-  courseRecommendations: (CourseRecommendation & { course: Course })[];
+  courseRecommendations: (CourseRecommendation & {
+    course: Course & {
+      instructor: {
+        firstName: string;
+        lastName: string;
+        username: string;
+      };
+      categories: {
+        name: string;
+      }[];
+      tags: {
+        name: string;
+      }[];
+    };
+  })[];
 };
 
 export default function RecommendedCourses({ courseRecommendations }: RecommendedCoursesProps) {
+  if (courseRecommendations.length === 0) {
+    return null;
+  }
+
   return (
-    <div className="bg-white shadow-md rounded-md p-4 mb-4">
-      <h2 className="text-xl font-semibold mb-4">Recommended Courses</h2>
-      {courseRecommendations.length === 0 ? (
-        <p className="text-gray-600">No course recommendations available.</p>
-      ) : (
-        <div className="grid grid-cols-1 gap-4">
-          {courseRecommendations.map((recommendation) => (
-            <CourseCard key={recommendation.id} course={recommendation.course} />
-          ))}
-        </div>
-      )}
+    <div>
+      <h2 className="text-2xl font-semibold mb-4">Recommended Courses</h2>
+      <div className="grid grid-cols-1 gap-4">
+        {courseRecommendations.map((recommendation) => (
+          <CourseCard key={recommendation.id} course={recommendation.course} />
+        ))}
+      </div>
     </div>
   );
 }
