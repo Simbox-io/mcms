a380fc41bfe81fdb21261aecdfa04bf2fb3de210
M1RL0K <mirlok@dr.com>
2024-03-31 06:57:17 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,13 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/", "/explore", "/api", "/api/posts", "/api/projects", "/api/files", "/api/spaces", "/api/pages", "/api/lms/courses", "/monitoring"],
+  afterAuth(auth, req, evt) {
+    // Handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+  }
 });
 
 export const config = {
