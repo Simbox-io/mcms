b9fb1680eb26b798527da3c0c0c20d59f9051499
Bill Wood <bwood@simbox.io>
2024-03-31 01:18:16 -0500

    More tweaks

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,15 @@
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
-    publicRoutes: ["/", "/explore", "/api", "/api/posts", "/api/projects", "/api/files", "/api/spaces", "/api/pages", "/api/lms/courses", "/monitoring"],
-    afterAuth(auth, req, evt) {
-      // Handle users who aren't authenticated
-      if (!auth.userId && !auth.isPublicRoute) {
-        return redirectToSignIn({ returnBackUrl: req.url });
-      }
+  publicRoutes: ["/", "/explore", "/api", "/api/posts", "/api/projects", "/api/files", "/api/spaces", "/api/pages", "/api/lms/courses", "/monitoring"],
+  afterAuth(auth, req, evt) {
+    // Handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
     }
-  });
- 
+  }
+});
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
