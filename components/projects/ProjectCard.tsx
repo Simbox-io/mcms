206e1e5deb298c50053be8abb50903503d016186
AlBlun <ABlundell@simbox.io>
2024-04-13 05:25:19 -0400

    Added client render back for router and user hooks

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Project } from '@/lib/prisma';
 import { Badge } from '@/components/ui/badge';
